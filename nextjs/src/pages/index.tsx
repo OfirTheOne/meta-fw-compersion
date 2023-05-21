e46741a9fc0ef1c7522039af286360094e2e8dfb
og8217 <og8217@intl.att.com>
2023-05-21 22:21:22 +0300

    add button usage

--- nextjs/src/pages/index.tsx
+++ nextjs/src/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
 import Link from '../components/Link';
 
 
@@ -20,6 +21,9 @@ export default function Page() {
         <Typography variant="h4" component="h1" gutterBottom>
         Next.js With Material UI - Asaf you pice of shit
         </Typography>
+        <Button variant="contained" onClick={() => alert('you pice of shit')} >
+          Click here and see alert
+        </Button>
         <Link href="/about" color="secondary">
           Go to the about page
         </Link>
